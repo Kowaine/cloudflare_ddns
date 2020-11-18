d849b07727d2178fc141687f06df5c40c9dc9e62
Kowaine <kowaine@qq.com>
2020-11-19 01:32:51 +0800

    添加日志记录（待测试）

--- ddns.py
+++ ddns.py
@@ -40,13 +40,27 @@ if __name__ == "__main__":
                 print("[ End {} ]".format(conf['name']))
                 print("------------")
             print("------ 结束更新 ------")
+            with open("record.log", "a") as f:
+                temp_time = time.localtime(time.time())
+                f.write("{}/{:0>2d}/{:0>2d} {:0>2d}:{:0>2d}:{:0>2d}".format(temp_time.tm_year, temp_time.tm_mon, temp_time.tm_mday, 
+            temp_time.tm_hour, temp_time.tm_min, temp_time.tm_sec), conf, "\n")
             print("将在{}s后开始下一次更新......".format(conf['interval']))
             time.sleep(conf['interval'])
-        except KeyboardInterrupt:
+        except KeyboardInterrupt as e:
             sys.stderr.write("\n[ Stopped! ]\n")
+            with open("error.log", "a") as f:
+                temp_time = time.localtime(time.time())
+                f.write("{}/{:0>2d}/{:0>2d} {:0>2d}:{:0>2d}:{:0>2d}".format(temp_time.tm_year, temp_time.tm_mon, temp_time.tm_mday, 
+            temp_time.tm_hour, temp_time.tm_min, temp_time.tm_sec), " ")
+                f.write(e, "\n")
             sys.exit(-1)
-        except requests.exceptions.RequestException:
+        except requests.exceptions.RequestException as e:
             sys.stderr.write("\n------ 连接超时，将在1分钟后重试! ------\n")
+            with open("error.log", "a") as f:
+                temp_time = time.localtime(time.time())
+                f.write("{}/{:0>2d}/{:0>2d} {:0>2d}:{:0>2d}:{:0>2d}".format(temp_time.tm_year, temp_time.tm_mon, temp_time.tm_mday, 
+            temp_time.tm_hour, temp_time.tm_min, temp_time.tm_sec), " ")
+                f.write(e, "\n")
             time.sleep(60)
         except Exception as e:
             sys.stderr.write("\n------ 遇到未知错误，将在1分钟后重试! ------\n")
