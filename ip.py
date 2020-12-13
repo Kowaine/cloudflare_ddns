6e13f6f55db1942faeae84a7d00dd47954e8827f
Kowaine <kowaine@qq.com>
2020-12-14 03:59:16 +0800

    恢复旧的方式以修复获取公网ip的bug

--- ip.py
+++ ip.py
@@ -16,7 +16,8 @@ def by_requests_wrapper(func):
         wrapped 包装后的get_ip_info, 使用requests :function()
     """
     def wrapped():
-        info = requests.get("http://ipv4.icanhazip.com")
+        info = requests.get("http://ipv4.icanhazip.com", timeout=3)
+        # info = requests.get("http://ip.chinaz.com/getip.aspx", timeout=3)
         info = info.text[:-1]
         return info
 
@@ -35,9 +36,10 @@ def by_socket_wrapper(func):
         try:
             s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
             s.connect(('223.5.5.5',80))
-            ip=s.getsockname()[0]
+            ip=s.getsockname()
         finally:
             s.close()
+        # ip = socket.gethostbyname_ex(socket.gethostname())
 
         return ip
 
@@ -45,7 +47,7 @@ def by_socket_wrapper(func):
 
 
 
-@by_socket_wrapper
+@by_requests_wrapper
 def get_ip_info():
     """
     用于解耦的占位函数，防止将来可能存在的方法更新影响其余的代码调用
